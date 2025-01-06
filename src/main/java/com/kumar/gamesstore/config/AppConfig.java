8e3e3a631d254ec64571fb2f957ecc4700d95fdb
Pankaj Kumar <c.thor269@gmail.com>
2025-01-06 21:44:39 +0530

    cors

--- src/main/java/com/kumar/gamesstore/config/AppConfig.java
+++ src/main/java/com/kumar/gamesstore/config/AppConfig.java
@@ -14,65 +14,59 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 
-
 @Configuration
+@EnableWebSecurity
 public class AppConfig {
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-	
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-		.authorizeHttpRequests(Authorize -> Authorize
-				.requestMatchers("/api/**").authenticated()
-				.anyRequest().permitAll()
-				)
-		.addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
-		.csrf().disable()
-		.cors().configurationSource(new CorsConfigurationSource() {
-					
-					@Override
-					public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-						
-						CorsConfiguration cfg = new CorsConfiguration();
-						
-						cfg.setAllowedOrigins(Arrays.asList(
-								
-								"https://frontend-prime-games.vercel.app",
-								"https://frontend-prime-games-git-main-pankaj-kumars-projects-4bc655b1.vercel.app"
-							)
-						);
-						//cfg.setAllowedMethods(Arrays.asList("GET", "POST","DELETE","PUT"));
-						cfg.setAllowedMethods(Collections.singletonList("*"));
-						cfg.setAllowCredentials(true);
-						cfg.setAllowedHeaders(Collections.singletonList("*"));
-						cfg.setExposedHeaders(Arrays.asList("Authorization"));
-						cfg.setMaxAge(3600L);
-						return cfg;
-						
-					}
-				})
-		.and()
-		.httpBasic()
-		.and()
-		.formLogin();
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(Authorize -> Authorize
+//                		.requestMatchers("/api/admin/**").hasAnyRole("SHOP_OWNER","ADMIN")
+                                .requestMatchers("/api/**").authenticated()
+                                .requestMatchers("/api/products/*/reviews").permitAll()
+                                .anyRequest().permitAll()
+                )
+                .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
+               
 		
 		return http.build();
 		
 	}
 	
-	@Bean
-	public PasswordEncoder passwordEncoder() {
+    // CORS Configuration
+    private CorsConfigurationSource corsConfigurationSource() {
+        return new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration cfg = new CorsConfiguration();
+                cfg.setAllowedOrigins(Arrays.asList("https://frontend-prime-games.vercel.app",
+                        "http://localhost:3000"));
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                cfg.setMaxAge(3600L);
+                return cfg;
+            }
+        };
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
