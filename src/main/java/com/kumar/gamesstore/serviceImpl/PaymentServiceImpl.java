93abdf28ab7955311e0290d882bf6287094d270f
Pankaj Kumar <c.thor269@gmail.com>
2025-01-31 16:07:19 +0530

    payment

--- src/main/java/com/kumar/gamesstore/serviceImpl/PaymentServiceImpl.java
+++ src/main/java/com/kumar/gamesstore/serviceImpl/PaymentServiceImpl.java
@@ -27,51 +27,47 @@ import com.stripe.param.checkout.SessionCreateParams;
 
 import lombok.RequiredArgsConstructor;
 
-
 @Service
 @RequiredArgsConstructor
-public class PaymentServiceImpl implements PaymentService{
+public class PaymentServiceImpl implements PaymentService {
 
-	 private final PaymentOrderRepository paymentOrderRepository;
-	    
-	 private final OrderRepository orderRepository;
-	    
-	 private final CartRepository cartRepository;
-	 
-     @Value("${stripe.api.key}")
-     private String stripeApiKey;
-     
-     @Value("${stripe.secret.key}")
-     private String stripeSecretKey;
-     
-     @Value("${razorpay.api.key}")
-     private String apiKey;
-     
-     @Value("${razorpay.api.secret}")
-     private String apiSecret;
-     
-			 
-	
-	@Override
-	public PaymentOrder createOrder(User user, Set<Order> orders) {
-		// TODO Auto-generated method stub
-		
-		Long amount=orders.stream().mapToLong(Order::getTotalSellingPrice).sum();
-        int couponPrice=cartRepository.findByUserId(user.getId()).getCouponPrice();
-        
-        PaymentOrder order=new PaymentOrder();
+    private final PaymentOrderRepository paymentOrderRepository;
+
+    private final OrderRepository orderRepository;
+
+    private final CartRepository cartRepository;
+
+    @Value("${stripe.api.key}")
+    private String stripeApiKey;
+
+    @Value("${stripe.secret.key}")
+    private String stripeSecretKey;
+
+    @Value("${razorpay.api.key}")
+    private String apiKey;
+
+    @Value("${razorpay.api.secret}")
+    private String apiSecret;
+
+    @Override
+    public PaymentOrder createOrder(User user, Set<Order> orders) {
+        // TODO Auto-generated method stub
+
+        Long amount = orders.stream().mapToLong(Order::getTotalSellingPrice).sum();
+        int couponPrice = cartRepository.findByUserId(user.getId()).getCouponPrice();
+
+        PaymentOrder order = new PaymentOrder();
         order.setUser(user);
-        order.setAmount(amount-couponPrice);
+        order.setAmount(amount - couponPrice);
         order.setOrders(orders);
         return paymentOrderRepository.save(order);
-	}
-	
+    }
 
     @Override
     public PaymentOrder getPaymentOrderById(Long id) throws Exception {
-        Optional<PaymentOrder> optionalPaymentOrder=paymentOrderRepository.findById(id);
-        if(optionalPaymentOrder.isEmpty()){
-            throw new Exception("payment order not found with id "+id);
+        Optional<PaymentOrder> optionalPaymentOrder = paymentOrderRepository.findById(id);
+        if (optionalPaymentOrder.isEmpty()) {
+            throw new Exception("payment order not found with id " + id);
         }
         return optionalPaymentOrder.get();
     }
@@ -81,132 +77,123 @@ public class PaymentServiceImpl implements PaymentService{
         PaymentOrder paymentOrder = paymentOrderRepository
                 .findByPaymentLinkId(paymentLinkId);
 
-        if(paymentOrder==null){
-            throw new Exception("payment order not found with id "+paymentLinkId);
+        if (paymentOrder == null) {
+            throw new Exception("payment order not found with id " + paymentLinkId);
         }
         return paymentOrder;
     }
 
-
     @Override
     public Boolean ProceedPaymentOrder(PaymentOrder paymentOrder,
-                                       String paymentId,
-                                       String paymentLinkId) throws RazorpayException {
+            String paymentId,
+            String paymentLinkId) throws RazorpayException {
 
-        if(paymentOrder.getStatus().equals(PaymentOrderStatus.PENDING)){
+        if (paymentOrder.getStatus().equals(PaymentOrderStatus.PENDING)) {
 
+            RazorpayClient razorpay = new RazorpayClient(apiKey, apiSecret);
+            Payment payment = razorpay.payments.fetch(paymentId);
 
-                RazorpayClient razorpay = new RazorpayClient(apiKey, apiSecret);
-                Payment payment = razorpay.payments.fetch(paymentId);
+            Integer amount = payment.get("amount");
+            String status = payment.get("status");
 
-                Integer amount = payment.get("amount");
-                String status = payment.get("status");
-
-                if(status.equals("captured")){
+            if (status.equals("captured")) {
 //                    System.out.println("payment ===== captured");
-                    Set<Order> orders=paymentOrder.getOrders();
-                    for(Order order:orders){
-                        order.setPaymentStatus(PaymentStatus.COMPLETED);
-                        orderRepository.save(order);
-                    }
-                    paymentOrder.setStatus(PaymentOrderStatus.SUCCESS);
-                    paymentOrderRepository.save(paymentOrder);
-
-
-                    return true;
+                Set<Order> orders = paymentOrder.getOrders();
+                for (Order order : orders) {
+                    order.setPaymentStatus(PaymentStatus.COMPLETED);
+                    orderRepository.save(order);
                 }
-                paymentOrder.setStatus(PaymentOrderStatus.FAILED);
+                paymentOrder.setStatus(PaymentOrderStatus.SUCCESS);
                 paymentOrderRepository.save(paymentOrder);
-                return false;
-            }
 
+                return true;
+            }
+            paymentOrder.setStatus(PaymentOrderStatus.FAILED);
+            paymentOrderRepository.save(paymentOrder);
             return false;
+        }
+
+        return false;
     }
-    
-	@Override
-	public PaymentLink createRazorpayPaymentLink(User user, Long Amount, Long orderId) throws RazorpayException {
-		// TODO Auto-generated method stub
-		
-		Long amount = Amount * 100;
-		
-		try {
-	        RazorpayClient razorpay = new RazorpayClient(apiKey, apiSecret);
+
+    @Override
+    public PaymentLink createRazorpayPaymentLink(User user, Long Amount, Long orderId) throws RazorpayException {
+        // TODO Auto-generated method stub
+
+        Long amount = Amount * 100;
+
+        try {
+            RazorpayClient razorpay = new RazorpayClient(apiKey, apiSecret);
 
             JSONObject paymentLinkRequest = new JSONObject();
-            paymentLinkRequest.put("amount",amount);
-            paymentLinkRequest.put("currency","INR");
-            
+            paymentLinkRequest.put("amount", amount);
+            paymentLinkRequest.put("currency", "INR");
+
             // Create a JSON object with the customer details
             JSONObject customer = new JSONObject();
-            customer.put("name",user.getFullName());
+            customer.put("name", user.getFullName());
 
-            customer.put("email",user.getEmail());
-            paymentLinkRequest.put("customer",customer);
+            customer.put("email", user.getEmail());
+            paymentLinkRequest.put("customer", customer);
 
             // Create a JSON object with the notification settings
             JSONObject notify = new JSONObject();
-            notify.put("email",true);
-            paymentLinkRequest.put("notify",notify);
+            notify.put("email", true);
+            paymentLinkRequest.put("notify", notify);
 
             // Set the reminder settings
-            paymentLinkRequest.put("reminder_enable",true);
-            
+            paymentLinkRequest.put("reminder_enable", true);
+
             // Set the callback URL and method
-            paymentLinkRequest.put("callback_url","http://localhost:3000/payment-success/"+orderId);
-            paymentLinkRequest.put("callback_method","get");
+            paymentLinkRequest.put("callback_url", "https://frontend-prime-games.vercel.app/payment-success/" + orderId);
+            paymentLinkRequest.put("callback_method", "get");
 
             PaymentLink payment = razorpay.paymentLink.create(paymentLinkRequest);
 
             String paymentLinkUrl = payment.get("short_url");
             String paymentLinkId = payment.get("id");
 
-
-
-            System.out.println("payment ----- "+payment);
+            System.out.println("payment ----- " + payment);
 
             return payment;
-			
-		} catch (RazorpayException e) {
+
+        } catch (RazorpayException e) {
 
             System.out.println("Error creating payment link: " + e.getMessage());
             throw new RazorpayException(e.getMessage());
         }
-	}
+    }
 
-	@Override
-	public String createStripePaymentLink(User user, Long amount, Long orderId) throws StripeException {
-		// TODO Auto-generated method stub
-		
-		Stripe.apiKey = stripeSecretKey;
-		
-		SessionCreateParams params = SessionCreateParams.builder()
-				   .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
-	                .setMode(SessionCreateParams.Mode.PAYMENT)
-	                .setSuccessUrl("http://localhost:3000/payment-success/"+orderId)
-	                .setCancelUrl("http://localhost:3000/payment/cancel")
-	                .addLineItem(SessionCreateParams.LineItem.builder()
-	                        .setQuantity(1L)
-	                        .setPriceData(SessionCreateParams.LineItem.PriceData.builder()
-	                                .setCurrency("usd")
-	                                .setUnitAmount(amount*100)
-	                                .setProductData(SessionCreateParams
-	                                        .LineItem
-	                                        .PriceData
-	                                        .ProductData
-	                                        .builder()
-	                                        .setName("Top up wallet")
-	                                        .build()
-	                                ).build()
-	                        ).build()
-	                ).build();
-		Session session = Session.create(params);
+    @Override
+    public String createStripePaymentLink(User user, Long amount, Long orderId) throws StripeException {
+        // TODO Auto-generated method stub
 
-	    System.out.println("session _____ " + session);
+        Stripe.apiKey = stripeSecretKey;
+
+        SessionCreateParams params = SessionCreateParams.builder()
+                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl("http://localhost:3000/payment-success/" + orderId)
+                .setCancelUrl("http://localhost:3000/payment/cancel")
+                .addLineItem(SessionCreateParams.LineItem.builder()
+                        .setQuantity(1L)
+                        .setPriceData(SessionCreateParams.LineItem.PriceData.builder()
+                                .setCurrency("usd")
+                                .setUnitAmount(amount * 100)
+                                .setProductData(SessionCreateParams.LineItem.PriceData.ProductData
+                                        .builder()
+                                        .setName("Top up wallet")
+                                        .build()
+                                ).build()
+                        ).build()
+                ).build();
+        Session session = Session.create(params);
+
+        System.out.println("session _____ " + session);
 
 //	        PaymentLinkResponse res = new PaymentLinkResponse();
 //	        res.setPayment_link_url(session.getUrl());
-
-	    return session.getUrl();
-	}
+        return session.getUrl();
+    }
 
 }
